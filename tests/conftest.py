a3a504f6405b70dcae46fd9891fc46927b7600a4
Naser Al Madi <nsalmadi@colby.edu>
2025-02-10 00:32:48 -0500

    fixing mock get_articles method

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ def test_client():
     with flask_app.test_client() as test_client:
         yield test_client # this is where the testing happens!
 
-# Mocking the get_articles function for testing
+
 def test_get_articles():
 
     articles = [{'source': {'id': None, 'name': 'Yahoo Entertainment'},
@@ -24,15 +24,16 @@ def test_get_articles():
                 'urlToImage': None,
                 'publishedAt': '2025-01-27T13:44:45Z',
                 'content': "If you click 'Accept all', we and our partners, including 239 who are part of the IAB Transparency &amp; Consent Framework, will also store and/or access information on a device (in other words, use … [+703 chars]"}]
-    
+
     return articles
 
-@pytest.fixture(scope='module')
-def news_api_mock_client():
+@pytest.fixture(scope='function')
+def news_api_mock_client(mocker):
     os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
     flask_app = create_app()
 
-    flask_app.blueprints['main'].get_articles = test_get_articles
+    # need to pip install pytest-mock for mocker to work
+    mocker.patch('website.views.get_articles', test_get_articles) 
 
     with flask_app.test_client() as test_client:
         yield test_client
--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -7,6 +7,7 @@ def test_home_page_mock_api(news_api_mock_client):
     THEN check the response is valid and contains the expected text
     """
     response = news_api_mock_client.get('/', follow_redirects=True)
+
     assert response.status_code == 200
     assert b'DeepSeek AI assistant' in response.data
     assert b'Yahoo Entertainment' in response.data
